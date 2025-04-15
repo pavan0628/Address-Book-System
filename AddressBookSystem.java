4f596a5ff56dd27002b7bc026b196097cdacca4d
pavan0628 <pavan.cse.rymec@gmail.com>
2025-04-15 15:36:21 +0530

    [PAVAN] Added : created a new class - Contacts by adding the contact details and methods - created a contact object in main class

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -5,6 +5,20 @@ public class AddressBookSystem {
 	//Main method
 	public static void main(String[] args) {
 		System.out.println("Welcome to the AddressBookSystem Program!");
+		
+		// Create a object - contact
+		Contact contact = new Contact(
+						"Pavan",
+						"M",
+						"Kapgal Road",
+						"Ballari",
+						"Karnataka",
+						583101,
+						7363653521L,
+						"pavan@gmail.com");
+
+		// Display the contact
+		System.out.println(contact);
 	}
 
 }
