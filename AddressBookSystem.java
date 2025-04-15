f124e91e04712d1c395823c4901a2810beac2aea
pavan0628 <pavan.cse.rymec@gmail.com>
2025-04-15 15:29:10 +0530

    Display welcome message and adding file

---
+++ AddressBookSystem.java
@@ -0,0 +1,10 @@
+package com.bl.opps2;
+
+public class AddressBookSystem {
+	
+	//Main method
+	public static void main(String[] args) {
+		System.out.println("Welcome to the AddressBookSystem Program!");
+	}
+
+}
